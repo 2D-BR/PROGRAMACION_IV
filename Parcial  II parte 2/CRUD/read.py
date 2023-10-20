a649e764f24c4928905706d479313ce193ac3d48
2D-BR <129999283+2D-BR@users.noreply.github.com>
2023-10-19 23:57:39 -0500

    Add files via upload

---
+++ Parcial  II parte 2/CRUD/read.py
@@ -0,0 +1,22 @@
+# read.py
+
+import sys
+sys.path.append('/Users/57304/Documents/PARCIAL II PARTE 2')
+
+from Modelos.Clientes import Cliente,clientes_registrados
+from Modelos.Facturas import Factura
+
+
+def leer_cliente(cedula):
+    if cedula in clientes_registrados:
+        return clientes_registrados[cedula]
+    else:
+        return None
+
+def leer_factura(cliente, fecha):
+    for factura in cliente.facturas:
+        if factura.fecha == fecha:
+            return factura
+    return None
+
+# Definir funciones para leer otros tipos de registros, como ControlPlagas, ControlFertilizantes, Antibioticos, etc.
---
+++ Parcial  II parte 2/CRUD/update.py
@@ -0,0 +1,27 @@
+# update.py
+
+import sys
+sys.path.append('/Users/57304/Documents/PARCIAL II PARTE 2')
+
+from Modelos.Clientes import Cliente,clientes_registrados
+from Modelos.Facturas import Factura
+
+
+
+
+
+def actualizar_cliente(cedula, nombre):
+    if cedula in clientes_registrados:
+        clientes_registrados[cedula].nombre = nombre
+        return True
+    else:
+        return False
+
+def actualizar_factura(cliente, fecha, nuevos_productos):
+    for factura in cliente.facturas:
+        if factura.fecha == fecha:
+            factura.productos_comprados = nuevos_productos
+            return True
+    return False
+
+# Definir funciones para actualizar otros tipos de registros, como ControlPlagas, ControlFertilizantes, Antibioticos, etc.
